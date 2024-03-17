95f003bc82d4f72afb94d012053008c6b9b25106
Santiago <agarcesb@unal.edu.co>
2024-03-17 13:46:58 -0500

    otra prueba de git en idea

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,4 +1,27 @@
 package gestion;
 
+import zooAnimales.Animal;
+import java.util.ArrayList;
+
 public class Zona {
+    private String nombre;
+    private Zoologico zoo;
+    private ArrayList<Animal> animales = new ArrayList<Animal>();
+
+    public Zona(String nombre, Zoologico zoo, ArrayList<Animal> animales) {
+        this.nombre = nombre;
+        this.zoo = zoo;
+        this.animales = animales;
+    }
+
+    public Zona() {
+    }
+
+    public void agregarAnimales(Animal animal) {
+        animales.add(animal);
+    }
+
+    public int cantidadAnimales() {
+        return animales.size();
+    }
 }
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,4 +1,21 @@
 package gestion;
 
+import java.util.ArrayList;
+
 public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private ArrayList<Zona> zonas = new ArrayList<Zona>();
+
+    public Zoologico(String nombre, String ubicacion, ArrayList<Zona> zonas) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.zonas = zonas;
+    }
+
+    public Zoologico() {}
+
+    public void agregarZonas(Zona zona) {
+        zonas.add(zona);
+    }
 }
